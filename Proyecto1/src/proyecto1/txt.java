02a2158ebcf8324dce80430ad4941425829e187e
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2022-05-28 16:43:44 -0400

    Creation of NewClass

---
+++ Proyecto1/src/proyecto1/txt.java
@@ -0,0 +1,12 @@
+
+package proyecto1;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.swing.JOptionPane;
+
+public class txt {
+    
+}
